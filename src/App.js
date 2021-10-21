1ff6951920d69ceccfb83bd8ea0985c05d514314
Naima Attaf <naimattaf@gmail.com>
2021-10-21 17:01:42 +0200

    create all components

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import "./App.css"
 import ShoppingCart from './components/ShoppingCart'
 
 const App = () => {
+
   return (
     <main className="container border mt-5 p-4">
       <section className="shopping-cart d-flex mx-auto">
-        {/* Shopping cart  */}
+        {/* the Shopping cart  */}
        <ShoppingCart/>
       </section>
     </main>
